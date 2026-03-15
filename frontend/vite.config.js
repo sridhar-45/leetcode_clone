d6aa4e9ad14d25b950b17b2d64013c5e674f1d2f
Sridhar Goudu <sridhargoudu7@gmail.com>
2026-03-15 20:11:50 +0530

    fix github pages build

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
 
 // export default defineConfig({
 //   plugins: [react()],
@@ -18,6 +18,10 @@ import react from '@vitejs/plugin-react'
 
 
 
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
 export default defineConfig({
-  base: '/leetcode_clone/',
+  plugins: [react()],
+  base: '/leetcode_clone/'
 })
