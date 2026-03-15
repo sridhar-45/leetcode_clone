48ff6a714280369622ab6e8511633f72088969a1
Sridhar Goudu <sridhargoudu7@gmail.com>
2026-03-15 18:57:01 +0530

    fix base path

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,16 +1,23 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// export default defineConfig({
+//   plugins: [react()],
+//   server: {
+//     port: 3000,           // Frontend runs on http://localhost:3000
+//     proxy: {
+//       '/api': {
+//         target: 'http://localhost:8000',  // Django backend
+//         changeOrigin: true,
+//         secure: false,
+//       }
+//     }
+//   }
+// })
+
+
+
+
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 3000,           // Frontend runs on http://localhost:3000
-    proxy: {
-      '/api': {
-        target: 'http://localhost:8000',  // Django backend
-        changeOrigin: true,
-        secure: false,
-      }
-    }
-  }
+  base: '/leetcode_clone/',
 })
